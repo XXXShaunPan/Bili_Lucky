318a91c0335a6f53cf8991fb45d911bbbe6eee16
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2021-06-28 18:10:57 +0800

    Update dynamic_redis.py

--- dynamic_redis.py
+++ dynamic_redis.py
@@ -1,23 +1,27 @@
 import redis
 import os
 
-Redis_psw=os.environ["Redis_psw"]
-Redis_IP=os.environ["Redis_IP"]
+# Redis_psw=os.environ["Redis_psw"]
+# Redis_IP=os.environ["Redis_IP"]
 
-rd=redis.Redis(host=Redis_IP, port=6379, db=0, password=Redis_psw)
+# rd=redis.Redis(host=Redis_IP, port=6379, db=0, password=Redis_psw)
 
 
-def save_dynamic(dynamic_id):
-	rd.lpush("already_dynamic_id-2", dynamic_id)
+def save_dynamic(dynamic_id,filename='dy_id1.txt'):
+	with open(filename,'a',encoding='utf-8') as f:
+		f.writelines(f'{dynamic_id}\n')
+# 	rd.lpush("already_dynamic_id-2", dynamic_id)
 
 
 def save_official_lucky(official_lucky):
 	rd.lpush("official_lucky_uid_dynamic", official_lucky)
 
 
-def get_dynamic():
-	res=rd.lrange("already_dynamic_id-2", 0, 3000)
-	return list(map(lambda x:str(x,encoding='utf-8'),res))
+def get_dynamic(filename='dy_id1.txt'):
+	with open(filename,'r',encoding='utf-8') as f:
+		return f.read()
+# 	res=rd.lrange("already_dynamic_id-2", 0, 3000)
+# 	return list(map(lambda x:str(x,encoding='utf-8'),res))
 
 # print(get_dynamic())
 
