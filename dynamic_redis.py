ee7e0726916c997f301923ab53b0af4cb59755bd
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2022-01-16 17:40:02 +0800

    Update dynamic_redis.py

--- dynamic_redis.py
+++ dynamic_redis.py
@@ -21,7 +21,7 @@ def save_official_lucky(official_lucky):
 def get_dynamic(filename='dy_id1.txt'):
 # 	with open(filename,'r',encoding='utf-8') as f:
 # 		return f.read().split('\n')[-1000:]
-	res=rd.lrange("already_dynamic_id", 0, 3000)
+	res=rd.lrange("already_dynamic_id", 0, 1000)
 	return list(map(lambda x:str(x,encoding='utf-8'),res))
 
 # print(get_dynamic())
